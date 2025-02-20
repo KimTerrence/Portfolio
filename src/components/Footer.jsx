a850ddab32a5a2c8a1ed885727fee372f34f2e48
Kim Terrence Quines <quineskimterrence@gmail.com>
2025-02-20 19:58:58 +0800

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@
 
 export default function Footer(){
     return(
-        <div className="w-full h-10 flex items-center justify-center">
+        <div className="w-full h-10 flex items-center flex-col justify-center">
             <p>Built by Kim Terrence S. Quines © 2023</p>
+            <p className="text-sm opacity-10">Design inpired by sezaru</p>
         </div>
     )
 };
