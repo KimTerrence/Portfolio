c4f2f91e7389f9f080133a9f277f1cd83d3c338d
Kim Terrence Quines <quineskimterrence@gmail.com>
2025-02-20 22:56:20 +0800

    fix max width

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@
 
 export default function Container({children}){
     return(
-        <div className="min-h-screen h-full bg-primary text-lightGray antialiased 2xl:px-60 font-poppins w-screen max-w-[1920px] top-0 left-0 right-0">
+        <div className="min-h-screen h-full bg-primary text-lightGray antialiased 2xl:px-60 font-poppins w-screen max-w-[1600px] top-0 left-0 right-0">
             {children}
         </div>
     )
